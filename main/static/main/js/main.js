dd7ee970e94d0f7e1c49a0e3fccc0d6555bb246d
ASAF <ilyin.rostislaw@gmail.com>
2024-08-15 10:01:32 +0300

    css 1.0

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,16 +1,13 @@
-const { createElement } = React;
-const { createRoot } = ReactDOM;
-
-// Создаем элемент input с placeholder и обработчиком события onClick
-const inputElement = createElement('input', {
-  placeholder: "Help text",
-  onClick: () => console.log("Clicked"),
-  onMouseOver: () => console.log("OVER")
-});
-
-// Получаем элемент контейнера в DOM
-const container = document.getElementById("app");
-
-// Создаем корень React и рендерим элемент
-const root = createRoot(container);
-root.render(inputElement);
+// const { createElement } = React;
+// const { createRoot } = ReactDOM;
+//
+// const inputElement = createElement('input', {
+//   placeholder: "Help text",
+//   onClick: () => console.log("Clicked"),
+//   onMouseOver: () => console.log("OVER")
+// });
+//
+// const container = document.getElementById("app");
+//
+// const root = createRoot(container);
+// root.render(inputElement);
