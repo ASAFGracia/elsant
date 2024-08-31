d3822ecbb5d132cbb897c17eb718747ddf6f4861
ASAF <ilyin.rostislaw@gmail.com>
2024-08-31 11:02:11 +0300

    2.0.1 стабильная версия с улучшенным списком услуг

--- main/views.py
+++ main/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    data = {
-        'title': 'Главная страница',
-        'values': ['Some', 'Hello', '123']
-    }
-    return render(request, 'main/index.html', data)
+    return render(request, 'main/index.html')
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
